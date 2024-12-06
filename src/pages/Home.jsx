6ecc1fb09c034f11d5542bf8f1f82fe1fb1a2a6a
Vitrolo <viniciusrabelodominguesdasilva@gmail.com>
2024-12-06 16:44:10 -0300

    pamonha

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -23,7 +23,7 @@ export default function Home() {
             <NavBar darkMode={darkMode} toggleDarkMode={toggleDarkMode} /> {/* Passando darkMode e toggleDarkMode */}
             <div className="home">
                 <h1 className="title-home">Tenha mais gestão e qualidade com o LhaBath!</h1>
-                <img src="./src/assets/banheiros.png" alt="Imagem de banheiros" />
+                <img src="../assets/banheiros.png" alt="Imagem de banheiros" />
                 <Link to={'/login'}><button>Entrar</button></Link>
                 <Link to={'/cadastro'}><button>Criar conta</button>  </Link>          
             </div>
