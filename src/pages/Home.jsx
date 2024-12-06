600d04843bd7c1aeb693e552efa43e47c224a207
Vitrolo <viniciusrabelodominguesdasilva@gmail.com>
2024-12-06 16:41:16 -0300

    pamonha

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import NavBar from "../components/NavBar"; // Importando o NavBar corretamente
 import { Link } from "react-router-dom";
+ 
 
 export default function Home() {
     const [darkMode, setDarkMode] = useState(false);
